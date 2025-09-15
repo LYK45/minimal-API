6d805ea5c2bf1b6d58f15c2652431953048c515c
Lucas Oliveira <lucasprado908@gmail.com>
2025-09-14 23:25:01 -0300

    Db

--- Infrestrutura/Db/DbContexto.cs
+++ Infrestrutura/Db/DbContexto.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"DESKTOP-7S6C415;Database=MinimalsDb;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
 }
