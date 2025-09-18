921b0e8b30d74e796bb12483fa369fc162fd68cb
Lucas Oliveira <lucasprado908@gmail.com>
2025-09-17 22:54:09 -0300

    Adm

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Infraestrutura.Db;
 using MinimalApi.DTOs;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
@@ -11,9 +16,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Quem ler sabe. que o Lucas é lindo e gostoso");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if (administradorServico.Login(loginDTO) != null)
     {
         return Results.Ok("Login feito com Sucesso!");
     }
